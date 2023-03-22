7b92cdc4b7709fd3b79ec066b0db041eea0b1576
Ryan Kadri <ryankadri.dev@gmail.com>
2023-03-22 10:03:04 +0100

    :lipstick: imports Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ShowMessagesComponent } from './show-messages/show-messages.component';
+import { MessagesService } from './message.service';
+import { MaterialModule } from './material-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowMessagesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [MessagesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
